6e38f6793c24aad49fd380215e3d96e3057d137a
Ankit Sharma <ankitsharmagh093@gmail.com>
2024-12-01 01:25:03 +0530

    navbar z-index fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <nav className="bg-white/10 backdrop-blur-lg border-white/20 border h-14 rounded-2xl flex items-center px-4 w-fit gap-8 sticky top-8 shadow-lg transition-all duration-300 hover:bg-white/15 hover:border-white/25">
+    <nav className="z-50 bg-white/10 backdrop-blur-lg border-white/20 border h-14 rounded-2xl flex items-center px-4 w-fit gap-8 sticky top-8 shadow-lg transition-all duration-300 hover:bg-white/15 hover:border-white/25">
       <a href="#home">
         <div className="flex gap-4 items-center">
           <div className="aspect-square rounded-full h-8 overflow-hidden">
